694c9be4a3c6a6676172421429ad1717df88b7d4
sposwal0231 <shivamposwal2004@gmail.com>
2025-04-20 21:40:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,9 +100,9 @@ else:
         # Add labels
         ax.text(f, -0.04, f"{100-i}", ha='center', va='top', 
                 fontsize=fontsize, fontweight=fontweight, color=color)  # C-axis
-        ax.text((1 + f)/2 + 0.03, (1 - f)*np.sqrt(3)/2, f"{100 - i}", ha='left', 
+        ax.text((1 + f)/2 + 0.03, (1 - f)*np.sqrt(3)/2, f"{i}", ha='left', 
                 fontsize=fontsize, fontweight=fontweight, color=color)  # A-axis
-        ax.text((1 - f)/2 - 0.03, (1 - f)*np.sqrt(3)/2, f"{i}", ha='right', 
+        ax.text((1 - f)/2 - 0.03, (1 - f)*np.sqrt(3)/2, f"{100-i}", ha='right', 
                 fontsize=fontsize, fontweight=fontweight, color=color)  # B-axis
 
     # User point
