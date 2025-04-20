412083aab950e247cda3a7f055e18e0b88c8d505
sposwal0231 <shivamposwal2004@gmail.com>
2025-04-21 03:19:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -93,8 +93,8 @@ for i in range(5, 100, 5):
     ax.plot([(1 - f)/2, 1 - f], [(1 - f)*np.sqrt(3)/2, 0], color=color, lw=lw, ls='-')
     # Axis labels
     ax.text(f, -0.04, f"{100 - i}", ha='center', va='top', fontsize=fontsize, fontweight=fontweight, color=color)
-    ax.text((1 + f)/2 + 0.03, (1 - f)*np.sqrt(3)/2, f"{100 - i}", ha='left', fontsize=fontsize, fontweight=fontweight, color=color)
-    ax.text((1 - f)/2 - 0.03, (1 - f)*np.sqrt(3)/2, f"{i}", ha='right', fontsize=fontsize, fontweight=fontweight, color=color)
+    ax.text((1 + f)/2 + 0.03, (1 - f)*np.sqrt(3)/2, f"{i}", ha='left', fontsize=fontsize, fontweight=fontweight, color=color)
+    ax.text((1 - f)/2 - 0.03, (1 - f)*np.sqrt(3)/2, f"{100- i}", ha='right', fontsize=fontsize, fontweight=fontweight, color=color)
 
 # User point
 ax.plot(x, y, 'ro', markersize=12, zorder=4)
