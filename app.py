4d333d9525999e3bc60824b17967fd94cecb2298
sposwal0231 <shivamposwal2004@gmail.com>
2025-04-20 20:48:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,9 +94,9 @@ else:
         ax.plot([(1 - f)/2, 1 - f], [(1 - f)*np.sqrt(3)/2, 0], color=color, lw=lw, ls=ls)
 
         # Labels
-        ax.text(f, -0.04, f"{100 - i}", ha='center', va='top', fontsize=fontsize, fontweight=fontweight, color=color)
-        ax.text((1 + f)/2 + 0.03, (1 - f)*np.sqrt(3)/2, f"{i}", ha='left', fontsize=fontsize, fontweight=fontweight, color=color)
-        ax.text((1 - f)/2 - 0.03, (1 - f)*np.sqrt(3)/2, f"{100 - i}", ha='right', fontsize=fontsize, fontweight=fontweight, color=color)
+        ax.text(f, -0.04, f"{i}", ha='center', va='top', fontsize=fontsize, fontweight=fontweight, color=color)
+        ax.text((1 + f)/2 + 0.03, (1 - f)*np.sqrt(3)/2, f"{100-i}", ha='left', fontsize=fontsize, fontweight=fontweight, color=color)
+        ax.text((1 - f)/2 - 0.03, (1 - f)*np.sqrt(3)/2, f"{i}", ha='right', fontsize=fontsize, fontweight=fontweight, color=color)
 
     # Plot user point
     ax.plot(x, y, 'ro', markersize=8)
